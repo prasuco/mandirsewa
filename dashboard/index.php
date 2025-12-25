c5b4d889968e59ea7ca74bf382f96451d5b3c38b
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-12-25 19:18:22 +0545

    minro

--- dashboard/index.php
+++ dashboard/index.php
@@ -50,22 +50,21 @@ include "../components/dashboard/header.php";
 <!-- if not current mandir, show a mandir selector modal -->
 <?php if ($mandirs && !$current_mandir) { ?>
     <form id="selectMandirModal"
-        class="modal bg-white w-full! max-w-2xl! rounded-lg p-6 space-y-4">
+        class="modal bg-white w-full! max-w-2xl!   rounded-lg p-6 space-y-4 ">
         <h3 class="text-base font-semibold text-gray-800">
             Please Select a Mandir
         </h3>
 
-        <div class="mandirs grid grid-cols-2 items-center gap-2">
+        <div class="mandirs grid grid-cols-2 md:grid-cols-3 max-h-96 overflow-auto  items-center gap-2  ">
             <?php foreach ($mandirs as $mandir) { ?>
                 <div class="w-full gap-2 p-2 flex flex-col shadow rounded-md items-center mandir  ">
                     <img class="w-20 h-20 rounded-full" src="/mandirsewa/<?= $mandir['logo'] ?>" alt="<?= $mandir['name'] ?> Logo">
                     <p> <?= $mandir['name'] ?> </p>
-                    <a class="btn-primary" href="/mandirsewa/dashboard/select-mandir.php?id=<?= $mandir['id'] ?>">
+                    <a class="btn-primary btn-sm" href="/mandirsewa/dashboard/select-mandir.php?id=<?= $mandir['id'] ?>">
                         Select Mandir
                     </a>
                 </div>
             <?php } ?>
-
 
         </div>
     </form>
