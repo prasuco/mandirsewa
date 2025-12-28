2a5743b9a423778932c2e6bb0eb03382e68cda53
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-12-28 10:24:27 +0545

    minor fix in registration.

--- register.php
+++ register.php
@@ -46,60 +46,60 @@ if ($_SERVER['REQUEST_METHOD'] == "POST") {
 ?>
 <div class="min-h-screen flex flex-col md:flex-row">
 
-  <!-- LEFT SIDE (IMAGE) -->
-  <div class="md:w-1/2 w-full h-64 md:h-auto">
-    <img src="/mandirsewa/public/images/login_hero.jpg" alt="Login Image" class="w-full h-full object-cover" />
-  </div>
-
-  <!-- RIGHT SIDE (LOGIN FORM) -->
-  <div class="md:w-1/2 w-full flex items-center justify-center bg-white">
-    <div class="w-full max-w-md p-8">
-
-      <h2 class="text-3xl font-bold text-orange-800 mb-6 text-center">
-        REGISTER YOUR MANDIR ORGANIZATION
-      </h2>
-      <form method="post" class="space-y-4">
-         <div>
-          <label class="block text-gray-600 mb-1">Name</label>
-          <input name="name" type="name" placeholder="Enter your name"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-
-        <div>
-          <label class="block text-gray-600 mb-1">Email</label>
-          <input name="email" type="email" placeholder="Enter your email"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-
-        <div>
-          <label class="block text-gray-600 mb-1">Password</label>
-          <input name="password" type="password" placeholder="Enter your password"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-        <div>
-          <label class="block text-gray-600 mb-1"> confirm Password</label>
-          <input name="password" type="password" placeholder="confirm password"
-            class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
-        </div>
-
-        <button type="submit" class="w-full bg-orange-600 text-white py-2 rounded-lg hover:bg-red-700 transition">
-          submit
-        </button>
-
-      </form>
-       <p class="text-center text-sm text-gray-500 mt-4">
-        Already have an account?
-        <a href="/mandirsewa/login.php" class="text-red-600 hover:underline">login</a>
-      </p>
-
-
-      
-
+    <!-- LEFT SIDE (IMAGE) -->
+    <div class="md:w-1/2 w-full h-64 md:h-auto">
+        <img src="/mandirsewa/public/images/login_hero.jpg" alt="Login Image" class="w-full h-full object-cover" />
     </div>
-  </div>
+
+    <!-- RIGHT SIDE (LOGIN FORM) -->
+    <div class="md:w-1/2 w-full flex items-center justify-center bg-white">
+        <div class="w-full max-w-md p-8">
+
+            <h2 class="text-3xl font-bold text-orange-800 mb-6 text-center">
+                REGISTER YOUR MANDIR ORGANIZATION
+            </h2>
+            <form method="post" class="space-y-4">
+                <div>
+                    <label class="block text-gray-600 mb-1">Name</label>
+                    <input name="name" type="name" placeholder="Enter your name"
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
+
+                <div>
+                    <label class="block text-gray-600 mb-1">Email</label>
+                    <input name="email" type="email" placeholder="Enter your email"
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
+
+                <div>
+                    <label class="block text-gray-600 mb-1">Password</label>
+                    <input name="password" type="password" placeholder="Enter your password"
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
+                <div>
+                    <label class="block text-gray-600 mb-1"> confirm Password</label>
+                    <input name="confirm_password" type="password" placeholder="confirm password"
+                        class="w-full px-4 py-2 border rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500" />
+                </div>
+
+                <button type="submit" class="w-full bg-orange-600 text-white py-2 rounded-lg hover:bg-red-700 transition">
+                    submit
+                </button>
+
+            </form>
+            <p class="text-center text-sm text-gray-500 mt-4">
+                Already have an account?
+                <a href="/mandirsewa/login.php" class="text-red-600 hover:underline">login</a>
+            </p>
+
+
+
+
+        </div>
+    </div>
 
 </div>
-        
+
 <?php
 include "components/footer.php";
 ?>
