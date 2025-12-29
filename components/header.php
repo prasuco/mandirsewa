45f7b900f339830f40e1bba0261b1fdff9d0b993
Prabin Subedi <42871240+prabincankod@users.noreply.github.com>
2025-12-29 08:54:11 +0545

    fix: using our own asset for fontawesome.

--- components/header.php
+++ components/header.php
@@ -13,30 +13,32 @@ require_once __DIR__ . "/../config/db.php";
         <?= $title; ?> | MandirSewa
     </title>
     <link rel="stylesheet" href="/mandirsewa/public/css/app.css?<?= time() ?>">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css"
-        integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw=="
-        crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="/mandirsewa/public/css/fontawesome.all.min.css" />
 
 </head>
 
-<style>
-
-
-</style>
 <body>
     <div class="bg-linear-to-b from-orange-50 to-white">
         <div class="max-w-7xl sticky  top-4 mx-auto mb-2 px-4">
             <div class="h-16 bg-white shadow rounded-2xl  px-6 flex items-center justify-between">
-
                 <div class="flex items-center gap-4">
-                    <img src="/mandirsewa/public/images/logo.webp" class="w-14 h-14 rounded-xl" alt="Mandir Logo" />
-                    <!-- <span class="font-semibold text-base">Mandir Sewa</span> -->
+                    <a href="/mandirsewa">
+                        <img src="/mandirsewa/public/images/logo.webp" class="w-14 h-14 rounded-xl" alt="Mandir Logo" />
+                    </a>
                 </div>
 
-                <a href="/mandirsewa/register.php" class="text-sm px-4 py-2 rounded-lg bg-primary text-white shadow-sm">
-                    Get started
-                    <i class="fas fa-arrow-right"></i>
-                </a>
+                <?php if (isset($_SESSION['id'])) { ?>
+                    <a href="/mandirsewa/dashboard" class="text-sm px-4 py-2 rounded-lg bg-primary text-white shadow-sm">
+                        Go to Dashboard
+                        <i class="fas fa-arrow-right"></i>
+                    </a>
+                <?php } else { ?>
+                    <a href="/mandirsewa/register.php" class="text-sm px-4 py-2 rounded-lg bg-primary text-white shadow-sm">
+                        Get started
+                        <i class="fas fa-arrow-right"></i>
+                    </a>
+                <?php } ?>
+
 
             </div>
         </div>
